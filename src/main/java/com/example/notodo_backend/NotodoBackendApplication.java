c88f64e424f79f5996d7bb5a1ef94db57759593b
kimyeonjin <kyj9750322@gmail.com>
2024-01-12 17:47:36 +0900

    first commit

---
+++ src/main/java/com/example/notodo_backend/NotodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.notodo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notodo_backend/NotodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notodo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
